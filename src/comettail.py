2f8d3d08c09f0f41d6b8add0a266db96a3d35590
Valeriy Zamarayev <valeriy.zamarayev@gmail.com>
2010-07-08 09:01:55 +0300

    interim

--- src/comettail.py
+++ src/comettail.py
@@ -2,14 +2,28 @@ from twisted.internet import reactor
 from twisted.application import service
 from twisted.web import server, resource
 
+class TailProtocol(process.ProcessProtocol):
+
+    """
+    a process protocol that receives the data from the process and fires a
+    given deferred
+    """
+
+    def __init__(self, deferred):
+        self.deferred = deferred
+
+    def childDataReceived(self, childFD, data):
+
 class FileMonitor:
     def __init__(self, filename):
         # run 'tail -f' subprocess
+        reactor.spawnProcess(
 
     def get_latest_lines(self):
         """
-
+        get the latest lines of the 'tail -f' subprocess
         """
+
 
 class MonitorResource(resource.Resource):
     isLeaf = True
